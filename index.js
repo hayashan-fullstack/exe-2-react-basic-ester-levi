9755321606b54e8cd8b20d616d7cb88f16762987
ester-levi <57223182+ester-levi@users.noreply.github.com>
2019-12-04 20:37:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+const Header = (props)=> {
+  return
+  (<h1>{props.course}</h1>)
+}
+const Content = (props)=> {
+  return( 
+  props.parts.map(elem => <Part part={[elem.name]}exercises={[elem.exercises]}/>)
+  
+  )
+}
+const Part=(props)=>{
+  return (
+  <p>{props.part}{props.exercises} </p>
+  )
+}
+const Total=(props)=>{
+  return(
+    <p>{props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
